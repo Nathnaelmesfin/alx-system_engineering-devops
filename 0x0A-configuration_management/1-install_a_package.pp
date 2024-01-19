f0be203b894991c8855f0ed870947017405ac35b
Nathnaelmesfin <nathnaelmesfin77@gmail.com>
2024-01-19 11:05:58 -0800

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# Define a package resource for Flask
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
-# Notify to refresh the shell so that 'flask' command is available immediately
-exec { 'Refresh Shell':
-  command     => '/bin/true',
-  refreshonly => true,
-  subscribe   => Package['Flask'],
-}
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
+}i
